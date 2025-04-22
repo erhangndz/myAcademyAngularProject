5e232497e8cbf25df791b2e3daee69ce8f875d8e
Erhan Gündüz <erhangndz@live.com>
2025-04-22 22:56:32 +0300

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './admin/category/category.component';
+import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { ProductComponent } from './admin/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+//Main Routes
+{path:'',component:MainLayoutComponent,children:[
+
+]},
+
+
+//Admin Routes
+{path:'admin',component:AdminLayoutComponent,children:[
+  {path:'category',component:CategoryComponent},
+  {path:'product',component:ProductComponent},
+]}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './admin/category/category.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { ProductComponent } from './admin/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    AdminLayoutComponent,
+    MainLayoutComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
