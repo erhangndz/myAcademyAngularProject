81d1d0250d5a37ac2a0c17321485c401f92e6960
Erhan Gündüz <erhangndz@live.com>
2025-04-25 21:47:21 +0300

    add category management with create, update, and delete functionalities using SweetAlert2

--- src/app/admin/category/category.component.ts
+++ src/app/admin/category/category.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Category } from '../../_models/category';
 import { CategoryService } from '../../_services/category.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-category',
@@ -11,6 +12,8 @@ import { CategoryService } from '../../_services/category.service';
 export class CategoryComponent {
 
 categoryList: Category[];
+category:Category = new Category();
+editCategory:any = {};
 
 constructor(private categoryService: CategoryService){
 this.getAll();
@@ -22,6 +25,71 @@ getAll(){
     error: err=> console.log(err)
   })
 }
+
+create(){
+  this.categoryService.create(this.category).subscribe({
+    next: value => this.categoryList.push(value),
+    error: err=> console.log(err),
+    complete: () =>  Swal.fire({
+      title: "Eklendi!",
+      text: "Kategori başarıyla eklendi.",
+      icon: "success"
+    })
+  })
+}
+
+onSelected(model:Category){
+  this.editCategory = model;
+}
+
+update(){
+  this.categoryService.update(this.editCategory.id,this.editCategory).subscribe({
+error: err => console.log(err),
+complete: () => {
+  Swal.fire({
+    title: "Güncellendi!",
+    text: "Kategori başarıyla güncellendi.",
+    icon: "success"
+  })
+  this.getAll()
+}
+  })
+}
+
+delete(id:number){
+
+  Swal.fire({
+    title: "Silmek istediğinize emin misiniz?",
+    text: "Bu işlemi geri alamazsınız!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Evet, Sil!",
+    cancelButtonText: "İptal"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      this.categoryService.delete(id).subscribe({
+        error: err=> console.log(err),
+        complete: () =>  {
+          Swal.fire({
+          title: "Silindi!",
+          text: "Kategori başarıyla silindi.",
+          icon: "success"
+        })
+      this.getAll()
+      }
+
+      })
+
+
+
+    }
+  });
+
+}
+
+
 
 
 
